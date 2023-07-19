00211228c74c20a928295849b0dd7278da1aef46
myektasenturk <107890995+myektasenturk@users.noreply.github.com>
2023-07-19 23:51:28 +0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,8 @@ const validator = require('../plugins/validator');
 const responses = require('../plugins/responses');
 
 const signUp = asyncHandler(async (req, res) => {
+    let data;
+    
     try {
         await validator.all(req.query, {
             email: 'required|email|unique:users,email',
@@ -15,10 +17,13 @@ const signUp = asyncHandler(async (req, res) => {
             res.status(400).send(responses.error(validator.errors(), 400));
             return;
         }
+
+        data = responses.success();
     } catch (error) {
-        res.status(200).send(responses.error(error));
-        return;
+        data = responses.error(error);
     }
+
+    res.status(200).send(data);
 });
 
 module.exports = { signUp };
