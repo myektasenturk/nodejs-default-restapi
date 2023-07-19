b3faf9d7654b0bab0503a1a31505284817bad535
myektasenturk <mertyektasenturk@gmail.com>
2023-07-19 23:48:24 +0300

    Validator: array, min, max, in, exist

--- plugins/validator.js
+++ plugins/validator.js
@@ -23,12 +23,10 @@ const all = async (params, rules) => {
 
       if (param === false) {
         control = 0;
+      } else if (params[key] === null) {
+        control = 0;
       } else {
-        if (params[key] === null) {
-          control = 0;
-        } else {
-          control = 1;
-        }
+        control = 1;
       }
     }
 
@@ -55,6 +53,12 @@ const all = async (params, rules) => {
               }
               break;
 
+            case "array":
+              if (!Array.isArray(params[key])) {
+                setError(key, "array");
+              }
+              break;
+
             case "boolean":
               if (typeof params[key] !== "boolean") {
                 setError(key, "boolean");
@@ -69,23 +73,57 @@ const all = async (params, rules) => {
               break;
 
             default:
-              if (rule.search('unique') >= 0 && params[key] !== undefined && params[key] !== null) {
+              if (rule.startsWith("in:")) {
+                let allowedValues = rule.substring(3).split(",");
+                if (!allowedValues.includes(params[key])) {
+                  setError(key, `in:${allowedValues.join(",")}`);
+                }
+              } else if (rule.startsWith("exist:")) {
+                if (rule.substring(6).split(",").length === 2 && params[key] !== undefined && params[key] !== null) {
+                  let [table, field] = rule.substring(6).split(",");
+                  let is = await knex(table).where(field, params[key]).first();
+
+                  if (is === undefined) {
+                    setError(key, `exist:${table},${field}`);
+                  }
+                } else {
+                  setError(key, "exist: insufficient data");
+                }
+              } else if (rule.search('unique') >= 0 && params[key] !== undefined && params[key] !== null) {
                 let uniqueArr = rule.split(":")[1].split(",");
 
                 if (uniqueArr.length === 4) {
                   let is = await knex(uniqueArr[0]).where(uniqueArr[1], params[key]).whereNot(uniqueArr[3], uniqueArr[2]).first();
 
-                  if (is !== undefined) {
+                  if (is === undefined) {
                     setError(key, "unique");
                   }
                 } else if (uniqueArr.length >= 2) {
                   let is = await knex(uniqueArr[0]).where(uniqueArr[1], params[key]).first();
 
-                  if (is !== undefined) {
+                  if (is === undefined) {
                     setError(key, "unique");
                   }
                 } else {
-                  setError(key, "unique");
+                  setError(key, "unique: insufficient data");
+                }
+              } else if (rule.startsWith("min:")) {
+                let minValue = parseInt(rule.split(":")[1]);
+                if (typeof params[key] === "number" && params[key] < minValue) {
+                  setError(key, `min:${minValue}`);
+                } else if (typeof params[key] === "string" && params[key].length < minValue) {
+                  setError(key, `minLength:${minValue}`);
+                } else if (Array.isArray(params[key]) && params[key].length < minValue) {
+                  setError(key, `minArrayLength:${minValue}`);
+                }
+              } else if (rule.startsWith("max:")) {
+                let maxValue = parseInt(rule.split(":")[1]);
+                if (typeof params[key] === "number" && params[key] > maxValue) {
+                  setError(key, `max:${maxValue}`);
+                } else if (typeof params[key] === "string" && params[key].length > maxValue) {
+                  setError(key, `maxLength:${maxValue}`);
+                } else if (Array.isArray(params[key]) && params[key].length > maxValue) {
+                  setError(key, `maxArrayLength:${maxValue}`);
                 }
               }
               break;
@@ -100,19 +138,15 @@ const fails = () => failStatus;
 const errors = () => errorList;
 
 const setError = (key, err) => {
-  if (!errorList.hasOwnProperty(key)) {
-    errorList[key] = [];
-  }
+  if (!errorList.hasOwnProperty(key)) { errorList[key] = []; }
 
   errorList[key].push(err);
-
-  failStatus = true;
+  
+  if (!failStatus) { failStatus = true; }
 };
 
-const validator = {
+module.exports = {
   all,
   fails,
-  errors,
+  errors
 };
-
-module.exports = validator;
